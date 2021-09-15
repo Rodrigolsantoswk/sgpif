190e160028fa5f9e68df32bcc580a8ed5368fec5
Rodrigo Lima <47004048+Rodrigolsantoswk@users.noreply.github.com>
2021-09-15 10:18:07 -0300

    Delete src directory

--- src/Interfaces/InterfaceCoordenadorDAO.java
+++
@@ -1,7 +0,0 @@
-package Interfaces;
-
-import java.util.List;
-
-public interface InterfaceCoordenadorDAO {
-        public List<classes.coordenador> selecionarCoordenadores();
-}
--- src/classesDAO/areaDAO.java
+++
@@ -1,5 +0,0 @@
-package classesDAO;
-
-public class areaDAO {
-    
-}
